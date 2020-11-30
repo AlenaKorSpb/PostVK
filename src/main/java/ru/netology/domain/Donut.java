bdf2d4cc8066dce993c4105d73894f8d8ac33146
Alena Korovina <evtam-29@mail.ru>
2020-11-30 22:34:54 +0300

    added 2 files

---
+++ src/main/java/ru/netology/domain/Donut.java
@@ -0,0 +1,58 @@
+package ru.netology.domain;
+
+public class Donut {
+    private boolean isDonut;
+    private int paidDuration;
+    private int placeholder;
+    private boolean canPublishFreeCopy;
+    private String editAll;
+    private int duration;
+
+    public boolean isDonut() {
+        return isDonut;
+    }
+
+    public void setDonut(boolean donut) {
+        isDonut = donut;
+    }
+
+    public int getPaidDuration() {
+        return paidDuration;
+    }
+
+    public void setPaidDuration(int paidDuration) {
+        this.paidDuration = paidDuration;
+    }
+
+    public int getPlaceholder() {
+        return placeholder;
+    }
+
+    public void setPlaceholder(int placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    public boolean isCanPublishFreeCopy() {
+        return canPublishFreeCopy;
+    }
+
+    public void setCanPublishFreeCopy(boolean canPublishFreeCopy) {
+        this.canPublishFreeCopy = canPublishFreeCopy;
+    }
+
+    public String getEditAll() {
+        return editAll;
+    }
+
+    public void setEditAll(String editAll) {
+        this.editAll = editAll;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -17,6 +17,20 @@ public class Post {
     private int isPinned;
     private int markedAsAds;
     private String copyright;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int signerId;
+    private boolean isFavorite;
+    private int postSource;
+    private int donut;
+    private int postponedId;
+
+
 
     public int getId() {
         return id;
@@ -136,6 +150,102 @@ public class Post {
 
     public void setCopyright(String copyright) {
         this.copyright = copyright;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(int postSource) {
+        this.postSource = postSource;
+    }
+
+    public int getDonut() {
+        return donut;
+    }
+
+    public void setDonut(int donut) {
+        this.donut = donut;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
     }
 }
 
